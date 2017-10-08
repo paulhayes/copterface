d855cbb1e380cb5adef47150eecc5517f6582a29
Paul Hayes <paul.hayes@entropedia.co.uk>
2017-10-08 23:25:52 +0100

    Improved example. Now includes working drift compensation and altitude correction.

--- example.js
+++ example.js
@@ -17,7 +17,7 @@ const FLYING_OK = "FLYING_OK";
 const CTRL_FLYING = "CTRL_FLYING";
 const videoOpts = {imageSize:"320x180"};
 const verticalSpeed = 0.5;
-const turnSpeed = 0.5;
+const turnSpeed = 0.25;
 
 
 const pngEncoder = new Client.PngEncoder(videoOpts);
@@ -42,28 +42,29 @@ var videoEventCallback = function(err) {
 
 var lastLED = 0;
 var navdata;
-var movement = {x:0,y:0};
+var movement = {x:0,y:0,z:0};
+var pcmd = {};
 var flying = false;
 var emergency = false;
-
-
-var copterface = Copterface(pngEncoder,{ outputImage:false },function(info){
+var bestFaceRect;
+var newFrameRecieved = false;
+var lastFrameTime = Date.now();
+var frameDelayTimeout = 1000;
+var copterface = Copterface(pngEncoder,{ outputImage:true  },function(info){
 	if( !navdata || !navdata.demo ){
-		movement.x = 0;
-		movement.y = 0;
-		move(movement);
 		return;
 	}
 
+	newFrameRecieved = true;
+	lastFrameTime = Date.now();
+
 	process.stdout.write('\x1B[2J\x1B[0f');
 	console.log("SPACE to takeoff and land. Debug face information can be viewed at http://localhost:8000");
-	var best = info.rects.reduce((a,b)=>(a && a.confidence>b.confidence)?a:b, null);
-	
+	console.log("Escpae does an emergency cut out. The C key does ground calibration");
+	var best = info.rects.reduce((a,b)=>(a && a.confidence>b.confidence)?a:b, null);	
 
 	console.log("Console State:"+navdata.demo.controlState+"   Fly State:"+navdata.demo.flyState+" battery:"+navdata.demo.batteryPercentage+"% ");
 	console.log("altitude:"+navdata.demo.altitude+" flying:"+navdata.droneState.flying);
-
-	
 	if( best && best.confidence > 0){
 		var time = Date.now();
 		if( ( time - lastLED ) > 1000 ){
@@ -71,67 +72,169 @@ var copterface = Copterface(pngEncoder,{ outputImage:false },function(info){
 			lastLED = time;
 		}
 		console.log(best);
-		
-		if( best.x < -0.1 ){
-			movement.x = turnSpeed;
-		}
-		else if(best.x > 0.1){
-			movement.x = -turnSpeed;
-		}
-		else {
-			movement.x = 0;
-		}
+				
+		bestFaceRect = best;
 
-		if( best.y < -0.1 ){
-			movement.y = -turnSpeed;
-		}
-		else if(best.y > 0.1){
-			movement.y = turnSpeed;
-		
-		}
-		else {
-			movement.y = 0;
-
-		}
 	}	
 	else {
 		console.log("no face detected");		
-		movement.x = 0;
+		//correctAltitude();
 
-		if( navdata.demo.altitude < 1 ){
-			movement.y = verticalSpeed;
-
-		}
-		else if( navdata.demo.altitude > 1.6 ){
-			movement.y = -verticalSpeed;
-		}
 	}
 
+	//driftCompensation();
 	
 
 	if(info.image){
 		pngServer.sendPng(info.image);
 	}
 
-	move(movement);
+	var cols = 80;
+	drawCentreHeading(cols, "  velocity  ");
+	drawCentreFloat( cols, "z="+Math.round( navdata.demo.velocity.x ) );
+	drawBar(cols, navdata.demo.velocity.x / 200);
+	drawCentreFloat( cols, "x="+Math.round(  navdata.demo.velocity.y ) );
+	drawBar(cols, navdata.demo.velocity.y / 200);
+	drawCentreFloat( cols, "y="+Math.round( navdata.demo.velocity.z ) );
+	drawBar(cols, navdata.demo.velocity.z / 200);
+	console.log("".padStart(cols,"#"));
 		
 });
 
-function move(movement){
+function drawCentreFloat(size,t){
+	var half = Math.round((size)/2);
+	console.log( (t).toString().padEnd(half," ").padStart(size," ") );
+}
+
+function drawCentreHeading(size,label){
+	console.log("".padStart(size/2-Math.floor(label.length/2),"#") +label+"".padStart(size/2-Math.ceil(label.length/2),"#"));
+	
+}
+
+function drawBar(size,t){
+	if( t > 1 ) t = 1;
+	if( t < -1 ) t = -1;
+	var half = Math.round((size-1)/2);
+	if( t<0 ) half++;
+	var padSide = ( t < 0 )? String.prototype.padStart : String.prototype.padEnd;
+	var oppositeSide = ( t >= 0 ) ? String.prototype.padStart : String.prototype.padEnd;
+	t = Math.abs(t);
+	console.log( oppositeSide.call(padSide.call(padSide.call("+",Math.round(half*t),"-"),half," "),size," ") );
+
+}
+
+function clearMovement(){
+	movement.x = 0;
+	movement.y = 0;
+	movement.z = 0;
+	movement.yaw = 0;
+}
+
+function move(){
 	//var ref = {  }
 	//control.ref(ref);
-	var pcmd = {};
-	if( true ){
-		pcmd[ ( movement.x > 0 ) ? "clockwise":"counterClockwise" ] = Math.abs(movement.x);
+	if( navdata && navdata.droneState.flying ){
+		pcmd[ ( movement.yaw > 0 ) ? "clockwise":"counterClockwise" ] = Math.abs(movement.yaw);
+		pcmd[ ( movement.x > 0 ) ? "right" : "left" ] = Math.abs(movement.x);
 		pcmd[ ( movement.y > 0 ) ? "up":"down" ] = Math.abs(movement.y);
+		pcmd[ ( movement.z > 0 ) ? "front" : "back" ] = Math.abs(movement.z);
+		console.log("movement:",pcmd);
 	}
-	console.log("movement:",pcmd);
 	control.ref({ fly:flying, emergency:emergency });
 	control.pcmd(pcmd);
 	control.flush();
+	pcmd = {};
+	
 }
 
-udpNavdatasStream.on('data',(d)=>navdata=d);
+function draw(){
+
+}
+
+udpNavdatasStream.on('data',(d)=>{
+	navdata=d;
+	update();
+});
+
+function update(){
+	var elapsed = Date.now() - lastFrameTime;
+
+	if( newFrameRecieved ){
+		
+		newFrameRecieved = false;		
+	}
+
+	if(elapsed > frameDelayTimeout){
+		flying = false;
+
+	}
+	correctAltitude();
+	driftCompensation();
+	lookAtFace();
+	move();	
+	clearMovement();
+
+}
+
+function lookAtFace(){
+		if( !bestFaceRect )
+			return;
+		var best = bestFaceRect;
+		bestFaceRect = null;
+
+		if( best.x < -0.1 ){
+			movement.yaw = -turnSpeed;
+		}
+		else if(best.x > 0.1){
+			movement.yaw = turnSpeed;
+		}
+		else {
+			movement.yaw = 0;
+		}
+
+		if( best.y < -0.1 ){
+			movement.y = turnSpeed;
+		}
+		else if(best.y > 0.1){
+			movement.y = -turnSpeed;
+		
+		}
+		else {
+			movement.y = 0;
+		}
+
+
+}
+
+function driftCompensation(){
+	if( navdata.demo ){
+		var compensationThresold = 50;
+		if( Math.abs( navdata.demo.velocity.x ) > compensationThresold ){
+			movement.z = -navdata.demo.velocity.x / 2000 ;
+		}
+		if( Math.abs(navdata.demo.velocity.y) > compensationThresold ){
+			movement.x = -navdata.demo.velocity.y / 2000 ;
+		}
+		
+			
+	}
+}
+
+function correctAltitude(){
+	if( !navdata.demo ){
+		return;
+	}
+
+	if( navdata.demo.altitude > 0 && navdata.demo.altitude < 1.2 ){
+		movement.y = verticalSpeed;
+	}
+	else if( navdata.demo.altitude > 1.8 ){
+		movement.y = -verticalSpeed;
+	}
+
+}
+
+
 udpNavdatasStream.resume();
 
 copterface.start();
@@ -159,9 +262,10 @@ process.stdin.on('keypress', (str, key) => {
 
   	if( navdata.droneState.emergencyLanding === 1 ){
   		var startTime = Date.now();
+  		console.log("Reseting drone");
+			
   		var intervalID = setInterval(()=>{
   			emergency = true;
-			console.log("Reseting drone");
 			control.config('general:navdata_demo', 'TRUE');
 			control.ref({ fly:flying, emergency:emergency });
 		  	control.flush();
